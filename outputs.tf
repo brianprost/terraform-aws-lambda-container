ef43fc2e7bb7aaf7479567d924e22fe463d3a975
Brian Prost <me@brianprost.com>
2025-04-24 12:11:59 -0400

    Add output for Lambda IAM role name

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,8 @@ output "lambda_role_arn" {
   description = "ARN of the Lambda IAM role"
   value       = aws_iam_role.lambda_role.arn
 }
+
+output "lambda_role_name" {
+  description = "Name of the Lambda IAM role"
+  value       = aws_iam_role.lambda_role.name
+}
